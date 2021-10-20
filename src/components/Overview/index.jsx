dbb153a93a8deb0fe784f76bf6e4aaa31b89dedc
msintaha <msintaha94@gmail.com>
2021-10-19 21:54:42 -0700

    Add upload button and parse csv

--- src/components/Overview/index.jsx
+++ src/components/Overview/index.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
 import * as d3 from 'd3';
 
-function Overview(props) {
+function Overview({ data }) {
     useEffect(() => {
       const margin = {top: 10, right: 30, bottom: 30, left: 60},
         width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        height = 600 - margin.top - margin.bottom;
     
       // append the svg object to the body of the page
       const svg = d3.select("#overview")
@@ -16,21 +18,20 @@ function Overview(props) {
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
       //Read the data
-      d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv").then( function(data) {
-
+      console.log(data);
       // Add X axis
       const x = d3.scaleLinear()
         .domain([0, 4000])
         .range([ 0, width ]);
-        svg.append("g")
+      svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x));
 
       // Add Y axis
-      const y = d3.scaleLinear()
+      const y = d3.scaleOrdinal()
         .domain([0, 500000])
         .range([ height, 0]);
-        svg.append("g")
+      svg.append("g")
         .call(d3.axisLeft(y));
 
       // Add dots
@@ -38,12 +39,10 @@ function Overview(props) {
       .selectAll("dot")
       .data(data)
       .join("circle")
-          .attr("cx", function (d) { return x(d.GrLivArea); } )
-          .attr("cy", function (d) { return y(d.SalePrice); } )
+          .attr("cx", function (d) { console.log(d); return x(d.Sex); } )
+          .attr("cy", function (d) { return y(d.Breed); } )
           .attr("r", 2)
           .style("fill", "#0065FF")
-
-      });
     }, []);
 
   return (
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Overview from '../../components/Overview';
+
+import * as d3 from 'd3';
+import { Alert, Button, Collapse } from '@mui/material';
 
 
 function Dashboard() {
+  const [showAlert, setShowAlert] = useState(false);
+  const [data, setData] = useState([]);
+
+  const onFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const text = e.target.result;
+      setData(d3.csvParse(text));
+      setShowAlert(true);
+    }
+
+    reader.readAsText(file);
+  }
+
   return (
     <div className="sm-Dashboard">
-      <Overview />
+      <Button
+        className="sm-Dashboard-upload"
+        size="small"
+        variant="contained"
+        component="label"
+      >
+        Upload File
+        <input
+          onChange={onFileUpload}
+          type="file"
+          accept=".csv"
+          hidden
+        />
+      </Button>
+      <br />
+      <Collapse in={showAlert}>
+        <Alert severity="success" onClose={() => setShowAlert(false)}>File uploaded successfully</Alert>
+      </Collapse>
+      {!!data.length && <Overview data={data} />}
     </div>
   );
 }

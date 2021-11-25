9e2d5bd4c41d2535e244ffeeefa99c32577f33d0
msintaha <msintaha94@gmail.com>
2021-11-24 20:47:44 -0800

    Fix backend cors issue

--- backend/src/index.py
+++ backend/src/index.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from livereload import Server
 from flask_cors import CORS, cross_origin
+import base64
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -12,6 +16,20 @@ if __name__ == '__main__':
 incomes = [
   { 'description': 'salary', 'amount': 5000 }
 ]
+
+
+
+@app.route('/api/generate-spectrogram', methods=['POST'])
+@cross_origin()
+def generate_spectrogram():
+  request_body = request.get_json()
+  print('Data', request_body)
+
+  filename = 'emoji.png' # Sample image in the src/ folder. This can be the dynamic image generated from librosa
+  image_file = open(os.path.join(dir_path, filename), "rb")
+  encoded_string = base64.b64encode(image_file.read())
+
+  return "data:image/jpg;base64," + encoded_string.decode('utf-8')
 
 @app.route('/')
 def get():

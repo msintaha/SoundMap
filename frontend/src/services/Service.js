e4be75163c1537664cee09dea82bf80308e7274b
msintaha <msintaha94@gmail.com>
2021-11-24 20:48:04 -0800

    Add sample endpoint to get data from backend

--- frontend/src/services/Service.js
+++ frontend/src/services/Service.js
@@ -4,4 +4,8 @@ export default class Service {
   static list() {
     return http.get('').then(response => response.data);
   }
+
+  static generateSpectrogram(data) {
+    return http.post('/generate-spectrogram', data).then(response => response.data);
+  }
 }

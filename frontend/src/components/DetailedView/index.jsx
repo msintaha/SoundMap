6461595a412d6b274abdf58a443a54f11985f60a
msintaha <msintaha94@gmail.com>
2021-11-26 15:03:43 -0800

    Render spectrogram on click

--- frontend/src/components/DetailedView/index.jsx
+++ frontend/src/components/DetailedView/index.jsx
@@ -1,42 +1,27 @@
 import React, { useEffect, useState, useHasChanged } from 'react';
 import PropTypes from 'prop-types';
 import './_index.scss';
-import * as d3 from 'd3';
-import { lazy } from 'react';
 import Service from '../../services/Service';
 
 function DetailedView({ attributeTypes, data }) {
-    if (data == '') {
-        return null;
-    }
-
     const [image, setImage] = useState('');
     const [soundKey, setSoundKey] = useState(attributeTypes.listical[0]);
     const [soundData, setSoundData] = useState(getSoundData(soundKey, data));
 
-    
-
-    console.log("sound data");
-    console.log(soundData);
-
-    // for now just deal with the first file
-    // convert from string representation
-    var first_file = soundData.split(",").map(Number);
-
-    console.log("first file");
-    console.log(first_file);
-
     useEffect(() => {
-        Service.generateSpectrogram({ sound_data: first_file }).then(res => {
+        const soundData = data.file_data.split(',').map(Number);
+        console.log(soundData);
+        Service.generateSpectrogram({ sound_data: soundData }).then(res => {
+            console.log('received data from endpoint', data);
             setImage(res);
         });
-    }, []);
+    }, [data]);
 
     console.log("image", image);
 
   return (
       <div className="sm-DetailedView">
-          {<img src={image} />}
+          <img src={image} width="300" height="280" />
     </div>
   );
 }

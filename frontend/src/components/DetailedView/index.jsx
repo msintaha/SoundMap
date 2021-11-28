06c7bb7a7085fcf2415367f8a2c0d562e8088614
Liz Reid <emr237@mail.usask.ca>
2021-11-28 00:09:03 -0800

    Changed text so that only info about the current x and y axis are shown by the spectrogram, as well as info about the filter if there is a filter applied

--- frontend/src/components/DetailedView/index.jsx
+++ frontend/src/components/DetailedView/index.jsx
@@ -2,24 +2,21 @@ import React, { useEffect, useState, useHasChanged } from 'react';
 import PropTypes from 'prop-types';
 import './_index.scss';
 import Service from '../../services/Service';
-//import { text } from 'stream/consumers';
 
-function DetailedView({ attributeTypes, data }) {
+function DetailedView({ data, xAxisAttr, categoryToFilterBy, yAxisAttr}) {
     const [image, setImage] = useState('');
+    // note: current bug, when beeswarm is reloaded the emphasised circle radius reverts back
+    // also: should adjust the size of this and maybe also the summary view, since they are too big together to fit properly
 
-    // set the text value details
-    const ordinals = attributeTypes.ordinal;
-    const quantitative = attributeTypes.quantitative;
-
-    var text_string = "";
-
-    console.log("ordinals 0", data[ordinals[0]]);
-    for (var i = 0; i < ordinals.length; i++) {
-        text_string = text_string + ordinals[i] + ": " + String(data[ordinals[i]]) + "\n";
+    
+    // only show current selected x and y axis, and filter
+    var text_string = yAxisAttr + ": " + String(data[yAxisAttr]) + "\n";
+    console.log("filter", categoryToFilterBy);
+    if (categoryToFilterBy != "") {
+        text_string = text_string + categoryToFilterBy + ": " + String(data[categoryToFilterBy]) + "\n";
     }
-    for (var i = 0; i < quantitative.length; i++) {
-        text_string = text_string + quantitative[i] + ": " +  String(parseFloat(data[quantitative[i]]).toFixed(2)) + "\n";
-    }
+    text_string = text_string + xAxisAttr + ": " + String(parseFloat(data[xAxisAttr]).toFixed(2)) + "\n";
+
 
     useEffect(() => {
         const soundData = data.file_data.split(',').map(Number);

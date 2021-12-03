65a7a7e1646ba3a909a66e4ff6956a38e488325b
msintaha <msintaha94@gmail.com>
2021-12-02 19:37:08 -0800

    Add sloader when showing details

--- frontend/src/components/DetailedView/index.jsx
+++ frontend/src/components/DetailedView/index.jsx
@@ -1,38 +1,57 @@
-import React, { useEffect, useState, useHasChanged } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './_index.scss';
 import Service from '../../services/Service';
+import { Box, CircularProgress, Skeleton } from '@mui/material';
+import SkeletonLoader from '../SkeletonLoader';
 
 function DetailedView({ data, xAxisAttr, categoryToFilterBy, yAxisAttr}) {
-    const [image, setImage] = useState('');
+	const [image, setImage] = useState('');
+	const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-			const soundData = data.file_data.split(',').map(Number);
-			const sampleRate = Number(data.sample_rate);
-			Service.generateSpectrogram({ sound_data: soundData, sample_rate: sampleRate }).then(res => {
-					setImage(res);
-			});
-    }, [data]);
+	useEffect(() => {
+		setLoading(true);
+		const soundData = data.file_data.split(',').map(Number);
+		const sampleRate = Number(data.sample_rate);
+		Service.generateSpectrogram({ sound_data: soundData, sample_rate: sampleRate }).then(res => {
+			setImage(res);
+			setLoading(false);
+		});
+	}, [data]);
 
-  return (
+  return isLoading ? <SkeletonLoader /> : (
     <div className="sm-DetailedView">
 			<img src={image} className="sm-DetailedView-spectrogram" />
 			<div className="sm-DetailedView-metadata">
-					<div><strong>{yAxisAttr}:</strong> {data[yAxisAttr]}</div>
-					<div><strong>{xAxisAttr}:</strong> {parseFloat(data[xAxisAttr]).toFixed(2)}</div>
-					<div><strong>{categoryToFilterBy}:</strong> {data[categoryToFilterBy]}</div>
+				<h6>Details</h6>
+				<hr />
+				<div><strong>{yAxisAttr}:</strong> {data[yAxisAttr]}</div>
+				<div><strong>{xAxisAttr}:</strong> {parseFloat(data[xAxisAttr]).toFixed(2)}</div>
+				<div><strong>{categoryToFilterBy}:</strong> {data[categoryToFilterBy]}</div>
 			</div>
     </div>
   );
+
+	// return (
+	// 	<div>
+	// 			<SkeletonLoader />
+	// 			<div className="sm-DetailedView">
+	// 				<img src={image} className="sm-DetailedView-spectrogram" />
+	// 				<div className="sm-DetailedView-metadata">
+	// 					<div><strong>{yAxisAttr}:</strong> {data[yAxisAttr]}</div>
+	// 					<div><strong>{xAxisAttr}:</strong> {parseFloat(data[xAxisAttr]).toFixed(2)}</div>
+	// 					<div><strong>{categoryToFilterBy}:</strong> {data[categoryToFilterBy]}</div>
+	// 				</div>
+	// 			</div>
+	// 	</div>
+	// )
 }
 
 
 DetailedView.propTypes = {
-	attributeTypes: PropTypes.shape({
-			listical: PropTypes.array,
-			ordinal: PropTypes.array,
-			quantitative: PropTypes.array,
-	}),
+	xAxisAttr: PropTypes.string,
+	yAxisAttr: PropTypes.string,
+	categoryToFilterBy: PropTypes.string,
 	data: PropTypes.object,
 }
 
